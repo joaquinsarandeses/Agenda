0454034ea0c1d309440c4dc5f2c1486953dd7b01
joaquinsarandeses <joaquinsarandeses@gmail.com>
2023-01-10 19:33:12 +0100

    Initial Commit

---
+++ Agenda/AgendaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgendaApp.swift
+//  Agenda
+//
+//  Created by joaquin sarandeses on 10/1/23.
+//
+
+import SwiftUI
+
+@main
+struct AgendaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
